e2775441d19c6d3bb3e3584480ec17d83a6cefa8
sandhyareddy451 <vemula.sandhya51@gmail.com>
2025-02-04 11:19:21 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Bappy",
+    author_email="entbappy73@gmail.com",
+    packages=find_packages()
+)
